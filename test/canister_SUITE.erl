07e613c44c5ebc5f09fdac5e7908246e4757f085
Jesse Gumm <gumm@sigma-star.com>
2025-12-26 10:49:53 -0600

    Rework from ct_slave to peer. Tests not yet passing.

--- test/canister_SUITE.erl
+++ test/canister_SUITE.erl
@@ -18,30 +18,33 @@
     netsplit/1
 ]).
 
+%Rework this whole module to use peer:start(#{connection=>standard_io})
+%Look at this: https://max-au.com/2022/01/06/peer/
+
 start_nodes(NumNodes) ->
     [start_node(Num) || Num <- lists:seq(1, NumNodes)].
 
-start_node(1) ->
-    application:ensure_all_started(canister),
-    {undefined, node()};
 start_node(_) ->
     Opts = #{
         wait_boot => 5000,
         name => peer:random_name(),
-        host => "127.0.0.1"
+        host => "127.0.0.1",
+        connection => standard_io
     },
     {ok, Peer, Node} = ?CT_PEER(Opts),
     unlink(Peer),
     io:format("Peer: ~p. Node: ~p~n",[Peer, Node]),
 
-    true = net_kernel:connect_node(Node),
+    %true = net_kernel:connect_node(Node),
 
     CodePath = lists:filter(fun(Path) ->
         nomatch =:= string:find(Path, "rebar3")
     end, code:get_path()),
-    true = rpc:call(Node, code, set_path, [CodePath]),
+    %peer:
+    true = peer:call(Peer, code, set_path, [CodePath]),
 
-    {ok, _} = erpc:call(Node, application, ensure_all_started, [canister]),
+    %{ok, _} = erpc:call(Node, application, ensure_all_started, [canister]),
+    {ok, _} = peer:call(Peer, application, ensure_all_started, [canister]),
     {Peer, Node}.
 
 %start_unconnected(Num) ->
@@ -49,9 +52,9 @@ start_node(_) ->
 %    erlang:disconnect_node(Node),
 %    Node.
 
-kill_peers([undefined|T]) ->
-    application:stop(canister),
-    kill_peers(T);
+%kill_peers([undefined|T]) ->
+%    application:stop(canister),
+%    kill_peers(T);
 kill_peers([H|T]) ->
     peer:stop(H),
     kill_peers(T);
@@ -104,7 +107,7 @@ init_per_group(Group, Config) ->
     NumNodes = group_to_num(Group),
     PeerNodes = start_nodes(NumNodes),
     {Peers, Nodes} = lists:unzip(PeerNodes),
-    sleep_and_show_sync_status(Nodes, 1, 30),
+    sleep_and_show_sync_status(Peers, Nodes, 1, 30),
     [{peer_nodes, PeerNodes}, {nodes, Nodes}, {peers, Peers} | Config].
 
 end_per_group(_Group, Config) ->
@@ -112,26 +115,30 @@ end_per_group(_Group, Config) ->
     kill_peers(Peers),
     Config.
 
-no_session(_Config) ->
+no_session(Config) ->
     ID = crypto:strong_rand_bytes(50),
     Key = crypto:strong_rand_bytes(50),
-    undefined = canister:get(ID, Key).
+    undefined = exec_peer(Config, canister, get, [ID, Key]).
+    %undefined = canister:get(ID, Key).
 
-no_key(_Config) ->
+no_key(Config) ->
     Sessions = rand_sessions(1),
-    ok = store_sessions(Sessions),
+    ok = store_sessions(Config, Sessions),
     [{ID, _}] = Sessions,
     Key = crypto:strong_rand_bytes(50),
-    undefined = canister:get(ID, Key).
+    undefined = exec_peer(Config, canister, get, [ID, Key]).
+    %undefined = canister:get(ID, Key).
 
 basic_crud(Config) ->
     Sessions = rand_sessions(),
     io:format("Generated ~p Sessions~n",[length(Sessions)]),
-    ok = store_sessions(Sessions),
-    Nodes = proplists:get_value(nodes, Config),
-    print_local_sessions(Nodes),
-    ok = check_sessions_dist(Nodes, Sessions),
-    ok = check_sessions_local(Nodes, Sessions).
+    ok = store_sessions(Config, Sessions),
+    %Nodes = proplists:get_value(nodes, Config),
+    Peers = proplists:get_value(peers, Config),
+    %Peers = proplists:get_value(peers, Config),
+    print_local_sessions(Peers),
+    ok = check_sessions_dist(Peers, Sessions),
+    ok = check_sessions_local(Peers, Sessions).
 
 add_one_node(Config) ->
     add_x_nodes(1, Config).
@@ -141,83 +148,103 @@ add_three_nodes(Config) ->
 
 add_x_nodes(Num, Config) ->
     Sessions = rand_sessions(),
-    ok = store_sessions(Sessions),
+    ok = store_sessions(Config, Sessions),
     Nodes = proplists:get_value(nodes, Config),
+    Peers = proplists:get_value(peers, Config),
     PeerNodes = add_new_nodes(Num),
-    {_Peers, NewNodes} = lists:unzip(PeerNodes),
+    {NewPeers, NewNodes} = lists:unzip(PeerNodes),
     Nodes2 = Nodes ++ NewNodes,
-    sleep_and_show_sync_status(Nodes2, 1, 30*Num),
-    ok = check_sessions_local(Nodes2, Sessions).
+    Peers2 = Peers ++ NewPeers,
+    sleep_and_show_sync_status(Peers2, Nodes2, 1, 30*Num),
+    ok = check_sessions_local(Peers2, Sessions).
 
 netsplit(Config) ->
     Sessions = rand_sessions(),
-    ok = store_sessions(Sessions),
+    ok = store_sessions(Config, Sessions),
     Nodes = proplists:get_value(nodes, Config),
+    Peers = proplists:get_value(peers, Config),
     [FirstSession,SecondSession|_] = Sessions,
     {ID1, KV1} = FirstSession,
     {Key1, _} = hd(KV1),
-    FirstAccess1 = canister:last_access_time(ID1),
+
+    FirstAccess1 = exec_first(Config, canister, last_access_time, [ID1]),
+    %FirstAccess1 = canister:last_access_time(ID1),
     {ID2, KV2} = SecondSession,
     {Key2, _} = hd(KV2),
-    FirstUpdate2 = canister:last_update_time(ID2),
+    FirstUpdate2 = exec_first(Config, canister, last_update_time, [ID2]),
+    %FirstUpdate2 = canister:last_update_time(ID2),
+
 
     DownNode = lists:last(Nodes),
-    erlang:disconnect_node(DownNode),
-    canister:get(ID1, Key1),
-    NextAccess1 = canister:last_access_time(ID1),
+    exec_first(Config, erlang, disconnect_node, [DownNode]),
+    %erlang:disconnect_node(DownNode),
+    exec_first(Config, canister, get, [ID1, Key1]),
+    %canister:get(ID1, Key1),
+    NextAccess1 = exec_first(Config, canister, last_access_time, [ID1]),
+    %NextAccess1 = canister:last_access_time(ID1),
 
     NewV2 = new_value,
-    FirstUpdate2 = canister:last_update_time(ID2),
+    FirstUpdate2 = exec_first(Config, canister, last_update_time, [ID2]),
+    %FirstUpdate2 = canister:last_update_time(ID2),
 
-    canister:put(ID2, Key2, NewV2),
-    NextUpdate2 = canister:last_update_time(ID2),
+    %canister:put(ID2, Key2, NewV2),
+    exec_first(Config, canister, put, [ID2, Key2, NewV2]),
 
-    net_kernel:connect_node(DownNode),
-    sleep_and_show_sync_status(Nodes, 1, 30),
+    %NextUpdate2 = canister:last_update_time(ID2),
+    NextUpdate2 = exec_first(Config, canister, last_update_time,[ID2]),
+
+    exec_first(Config, net_kernel, connect_node, [DownNode]),
+    %net_kernel:connect_node(DownNode),
+    %
+    sleep_and_show_sync_status(Peers, Nodes, 1, 30),
 
 
-    SyncedAccessTime1 = erpc:call(DownNode, canister, last_access_time, [ID1]),
+    SyncedAccessTime1 = exec_last(Config, canister, last_access_time, [ID1]),
 
-    print_times("Access Times", FirstAccess1, NextAccess1, SyncedAccessTime1),
+    print_times(Config, "Access Times", FirstAccess1, NextAccess1, SyncedAccessTime1),
 
     SyncedAccessTime1 > FirstAccess1 orelse exit(access_time_unchanged),
     SyncedAccessTime1 == NextAccess1 orelse exit({access_time_not_synced, [SyncedAccessTime1, NextAccess1]}),
 
-    SyncedUpdateTime2 = erpc:call(DownNode, canister, last_update_time, [ID2]),
+    SyncedUpdateTime2 = exec_last(Config, canister, last_update_time, [ID2]),
 
-    print_times("Update Times", FirstUpdate2, NextUpdate2, SyncedUpdateTime2),
+    print_times(Config, "Update Times", FirstUpdate2, NextUpdate2, SyncedUpdateTime2),
 
     SyncedUpdateTime2 > FirstUpdate2 orelse exit(update_time_unchanged),
     SyncedUpdateTime2 == NextUpdate2 orelse exit({update_time_not_synced, [SyncedUpdateTime2, NextUpdate2]}),
 
-    SyncedNewV2 = erpc:call(DownNode, canister, get_local, [ID2, Key2]),
+    SyncedNewV2 = exec_last(Config, canister, get_local, [ID2, Key2]),
 
     SyncedNewV2==NewV2 orelse exit({updated_value_not_synced, [NewV2, SyncedNewV2]}).
 
 
-print_times(Label, Orig, New, OnTargetNode) ->
-    canister_log:info("~s:~nOrig: ~p~nNew: ~p~nPost-resynced on target node: ~p", [Label, Orig, New, OnTargetNode]).
+print_times(Config, Label, Orig, New, OnTargetNode) ->
+    LogArgs = ["~s:~nOrig: ~p~nNew: ~p~nPost-resynced on target node: ~p", [Label, Orig, New, OnTargetNode]],
+    exec_first(Config, canister_log, info, LogArgs).
 
-sleep_and_show_sync_status(Nodes, Secs, Times) ->
-    print_local_sessions(Nodes),
-    print_running_resync(Nodes),
+sleep_and_show_sync_status(Peers, Nodes, Secs, Times) ->
+    print_local_sessions(Peers),
+    print_running_resync(Nodes, Peers),
     lists:foreach(fun(X) ->
         canister_log:info("Sleeping ~ps (~p/~p)~n",[Secs, X, Times]),
         timer:sleep(Secs * 1000),
-        print_local_sessions(Nodes),
-        print_running_resync(Nodes)
+        print_local_sessions(Peers),
+        print_running_resync(Nodes, Peers)
     end, lists:seq(1, Times)).
 
-print_local_sessions(Nodes) ->
-    lists:foreach(fun(Node) ->
-        N = erpc:call(Node, canister, num_local_sessions, []),
-        canister_log:info("Number of Local Session on ~p: ~p",[Node, N])
-    end, Nodes).
+print_local_sessions(Peers) ->
+    lists:foreach(fun(Peer) ->
+        N = peer:call(Peer, canister, num_local_sessions, []),
+        %N = erpc:call(, canister, num_local_sessions, []),
+        canister_log:info("Number of Local Session on ~p: ~p",[Peer, N])
+    end, Peers).
 
-print_running_resync(Nodes) ->
+print_running_resync(Nodes, [Peer|_]) ->
     lists:foreach(fun(Node) ->
-        Syncing = canister_resync:is_resyncing(Node),
-        Queued = canister_resync:num_queued(Node),
+        Syncing = peer:call(Peer, canister_resync, is_resyncing, [Node]),
+        %Syncing = canister_resync:is_resyncing(Node),
+        Queued = peer:call(Peer, canister_resync, num_queued, [Node]),
+        %Queued = canister_resync:num_queued(Node),
         canister_log:info("Node (~p) Resyncing: ~p (~p queued)",[Node, Syncing, Queued])
     end, Nodes).
 
@@ -225,40 +252,40 @@ add_new_nodes(NumNodes) ->
     [start_node(Num+100) || Num <- lists:seq(1, NumNodes)].
 
 
-store_sessions([]) ->
+store_sessions(_Config, []) ->
     ok;
-store_sessions([{ID, KV} | Rest]) ->
+store_sessions(Config, [{ID, KV} | Rest]) ->
     lists:foreach(fun({Key, Val}) ->
-        undefined = canister:put(ID, Key, Val)
+        undefined = exec_peer(Config, canister, put, [ID, Key, Val])
     end, KV),
-    store_sessions(Rest).
+    store_sessions(Config, Rest).
 
-check_sessions_local(Nodes, Sessions) ->
-    check_sessions(get_local, Nodes, Sessions).
+check_sessions_local(Peers, Sessions) ->
+    check_sessions(get_local, Peers, Sessions).
 
-check_sessions_dist(Nodes, Sessions) ->
-    check_sessions(get, Nodes, Sessions).
+check_sessions_dist(Peers, Sessions) ->
+    check_sessions(get, Peers, Sessions).
 
 check_sessions(_Fun, _, []) ->
     ok;
-check_sessions(Fun, Nodes, [{ID, KV} | Rest]) ->
+check_sessions(Fun, Peers, [{ID, KV} | Rest]) ->
     NumLeft = length(Rest),
     case NumLeft rem 100 of
         0 -> canister_log:info("~p Session Checks Remaining",[NumLeft]);
         _ -> ok
     end,
-    ok = check_session(Fun, Nodes, ID, 1, KV),
-    check_sessions(Fun, Nodes, Rest).
+    ok = check_session(Fun, Peers, ID, 1, KV),
+    check_sessions(Fun, Peers, Rest).
 
 check_session(_, _, _, _, []) ->
     ok;
-check_session(Fun, Nodes, ID, I, [{K, V} | RestKV]) ->
-    lists:foreach(fun(Node) ->
-        case get_val(Node, Fun, ID, K) of
+check_session(Fun, Peers, ID, I, [{K, V} | RestKV]) ->
+    lists:foreach(fun(Peer) ->
+        case get_val(Peer, Fun, ID, K) of
             V -> ok;
             Other ->
                 exit({failed_lookup, [
-                    {node, Node},
+                    {peer, Peer},
                     {function, Fun},
                     {iteration,I},
                     {id, ID},
@@ -267,13 +294,14 @@ check_session(Fun, Nodes, ID, I, [{K, V} | RestKV]) ->
                     {returned_value, Other}
                 ]})
         end
-    end, Nodes),
-    check_session(Fun, Nodes, ID, I+1, RestKV).
+    end, Peers),
+    check_session(Fun, Peers, ID, I+1, RestKV).
 
-get_val(Node, Fun, ID, K) when Node==node() ->
-    canister:Fun(ID, K);
-get_val(Node, Fun, ID, K) ->
-    erpc:call(Node, canister, Fun, [ID, K]).
+%get_val(Peer, Fun, ID, K) when Node==node() ->
+%    canister:Fun(ID, K);
+get_val(Peer, Fun, ID, K) ->
+    peer:call(Peer, canister, Fun, [ID, K]).
+    %erpc:call(Node, canister, Fun, [ID, K]).
 
 
 print_check(Node, Fun, ID, Key, ExpectedVal) ->
@@ -295,6 +323,29 @@ rand_sessions(Num) ->
         KV2 = sets:to_list(sets:from_list(KV)),
         {ID, KV2}
     end, lists:seq(1, Num)).
+
+exec_peer(Config, Mod, Fun, Args) ->
+    Peers = proplists:get_value(peers, Config),
+    Peer = rand_item(Peers),
+    peer:call(Peer, Mod, Fun, Args).
+
+exec_first(Config, Mod, Fun, Args) ->
+    Peers = proplists:get_value(peers, Config),
+    Peer = hd(Peers),
+    peer:call(Peer, Mod, Fun, Args).
+
+exec_last(Config, Mod, Fun, Args) ->
+    Peers = proplists:get_value(peers, Config),
+    Peer = lists:last(Peers),
+    peer:call(Peer, Mod, Fun, Args).
+
+rand_item([X]) ->
+    X;
+rand_item(List) ->
+    Max = length(List),
+    I = rand:uniform(Max),
+    lists:nth(I, List).
+     
 
 rand_value() ->
     rand_value(rand:uniform(5)).
